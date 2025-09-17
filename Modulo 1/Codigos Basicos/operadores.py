cf03719ebe5ba4c6f8a3edfc9f03094301f8661e
ezequiel <ezequielmantuanosousadossantos@gmail.com>
2025-09-17 08:41:37 -0300

    operadores.py

---
+++ Modulo 1/Codigos Basicos/operadores.py
@@ -0,0 +1,18 @@
+# Aritméticos 
+Adição = 2 + 5
+print(Adição)
+
+Subtração = 4 - 4
+print(Adição)
+
+Multiplicação = 10 * 5
+print(Multiplicação)
+
+Divisão = 10 / 7
+print(Divisão)
+
+Potencia = 2 ** 2
+print(Potencia)
+
+Divisão_inteira = 22 // 44
+print(Divisão_inteira)
