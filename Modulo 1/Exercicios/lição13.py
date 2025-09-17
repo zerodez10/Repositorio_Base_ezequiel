c7617f7f1f6f458fd14ee301949e27ae461f6f2b
ezequiel <ezequielmantuanosousadossantos@gmail.com>
2025-09-17 09:51:47 -0300

    Create lição13.py

---
+++ Modulo 1/Exercicios/lição13.py
@@ -0,0 +1,13 @@
+Numero_Fl = float(input("numero quebrado:"))
+Numero_IN = int(input("numero inteiro:"))
+Texto_Str = str(input("Isso é um Str:"))
+logico = bool(input("Você tem um celular?:"))
+logico2 = bool(input("Você tem um celular?:"))
+
+print("-----------------")
+print(f"Float: {Numero_Fl}") 
+print(f"Int: {Numero_IN}")
+print(f"Str: {Texto_Str}")
+print(f"Bool: {logico}")
+print(f"Bool: {logico2}") 
+print("-----------------")
